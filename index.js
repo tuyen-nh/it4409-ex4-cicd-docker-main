b7e9e6bb96bcd98cb7aea8f7f8fe57889bc297d6
tuyen-nh~ <tuyenmexico@gmailcom>
2025-12-19 11:34:01 +0700

    oki3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-    res.status(200).send("Xin chào");
+    res.status(200).send("Xin chào bạn nhé");
 });
 
 // Start server
